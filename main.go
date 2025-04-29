f6a31edafefdc639c6287d93d2c8bf0195917a62
ilyamalakhov <ilyamalahov01@gmail.com>
2025-04-29 20:27:16 +0300

    second commit

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Привет!")
+	//var userHeight, userKg = 1.8,100
+	const IMTPower = 2
+	userHeight := 1.8
+	userKg := 100.0
+	IMT := userKg / math.Pow(userHeight, IMTPower)
+	fmt.Println(IMT)
 }
